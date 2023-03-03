608446d0e987ee274ec413f89c83d4336bce676b
Richa Purba <61378336+richapurba@users.noreply.github.com>
2023-03-03 10:38:00 +0000

    Load the exoplanets dataset

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 #Title and subtitle
 st.title("Habitable Exoplanets and Stars")
 st.subheader("NASA Discovery")
+
+#Load dataframe
+df = pd.read_excel('Habitable.csv')
+st.dataframe(df)
